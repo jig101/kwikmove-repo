8952ed269a761fecb875361bdab96bdd125903ad
jig101 <mrjp123@googlemail.com>
2016-10-19 19:01:47 +0100

    del

--- skin/config/config.rb
+++
@@ -1,8 +0,0 @@
-project_path    = "./"
-http_path       = "./"
-css_dir         = "css"
-sass_dir        = "sass"
-images_dir      = "images"
-fonts_dir       = "fonts"
-javascripts_dir = "js"
-relative_assets = true
